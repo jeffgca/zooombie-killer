b28739324ed853af92cf79d8dad18b421865f1db
Jeff Griffiths <jeff@eko-recordings.ca>
2022-05-18 16:22:39 -0700

    working prototype, including icons

--- readme.md
+++ readme.md
@@ -1,18 +1,13 @@
 # Zoombie Killer, qu'est-ce que c'est?
 
-
 > Rationale: if you spend all day on zoom calls, you will slowly accrue a bunch of tabs all pointed at the Zoom,us postattendee page. This extension kills these tabs in your browser with extreme prejudice.
 
 ![](/source/zoombie.jpg)
 
-### reference browser-extension-template
+## TODOs
 
-[link-webext-polyfill]: https://github.com/mozilla/webextension-polyfill
-[link-rgh]: https://github.com/sindresorhus/refined-github
-[link-ngh]: https://github.com/sindresorhus/notifier-for-github
-[link-hfog]: https://github.com/sindresorhus/hide-files-on-github
-[link-tsconfig]: https://github.com/sindresorhus/tsconfig
-[link-options-sync]: https://github.com/fregante/webext-options-sync
-[link-cws-keys]: https://github.com/DrewML/chrome-webstore-upload/blob/master/How%20to%20generate%20Google%20API%20keys.md
-[link-amo-keys]: https://addons.mozilla.org/en-US/developers/addon/api/key
+ - [x] Add Zooombie toolbar icon
+  - [x] update count every time we hit a zoom url
+  - [] persist storage of total count
+ - [] Embedded home page with data?
 
--- source/background.js
+++ source/background.js
@@ -1,12 +1,65 @@
-function listener(details) {
-  console.log('closing this zoombie', details);
-  browser.tabs.remove(details.tabId);
-} 
+// Async function / closure so we can use await
 
-browser.webNavigation.onDOMContentLoaded.addListener(
-  listener,
-  {url: [
-    { hostSuffix: "zoom.us" },
-    { pathContains: "/postattendee?" }
-  ]}
-);
+(async () => {
+
+  // functions
+
+  // handler to catch data updates and update the action badge
+  async function changeHandler(event) {
+    console.log('changeHandler', event);
+    await browser.browserAction.setBadgeText({ text: event.count.newValue });
+  }
+
+  async function listener(details) {
+    let current = await browser.browserAction.getBadgeText({});
+    let intCurrent = parseInt(current); // should only be an Int
+    let next = intCurrent + 1;
+    result.count = String(next);
+
+    console.log('result', result);
+
+    browser.storage.local.set(result);
+
+    // console.log('>>>', current, intCurrent, next);
+    // console.log('closing this zoombie', details);
+
+    setTimeout(async () => {
+      
+      await browser.tabs.remove(details.tabId);
+    }, 2000) // Pref?
+  }
+
+  // our extension homepage
+  const homePage = './zooombie.html';
+
+  // handle changes in the storage
+  browser.storage.onChanged.addListener(changeHandler);
+
+  // handle onclick by opening the extension page
+  browser.browserAction.onClicked.addListener(async (tab, data) => {
+    await browser.tabs.create({url: homePage });
+  });
+
+  // get some data from local storage
+  let result = await browser.storage.local.get();
+
+  if (!result || !result.count) {
+    result = {count: "0"};
+  }
+
+  // initially set the badge Text
+  browser.browserAction.setBadgeText({text: result.count});
+
+  // set the badge text background 
+  await browser.browserAction.setBadgeBackgroundColor({color: "rebeccapurple"});
+  
+  // handle when any tab loads the zoom postAttendee page eg
+  // https://us02web.zoom.us/postattendee?mn=[...]
+  browser.webNavigation.onDOMContentLoaded.addListener(
+    listener,
+    {url: [
+      { hostSuffix: "zoom.us" },
+      { pathContains: "/postattendee?" }
+    ]}
+  );
+})()
